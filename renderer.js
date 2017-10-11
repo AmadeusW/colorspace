111dea1389211da0c4a54a3ce77aaae087166041
Amadeusz Wieczorek <git@amadeusw.com>
2017-10-11 09:02:42 -0700

    rendering

--- renderer.js
+++ renderer.js
@@ -2,7 +2,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( 300, 300 );
 var initializeRenderer = function() {
     document.getElementById("renderer").appendChild( renderer.domElement );
 }
