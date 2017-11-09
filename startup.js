66e3a5d574406942e8c8b100aca0fda326da4fff
Amadeusz Wieczorek <git@amadeusw.com>
2017-11-09 08:16:31 -0800

    moves all images to viewmodel

--- startup.js
+++ startup.js
@@ -13,9 +13,44 @@ var sampleImages = new Vue({
                 url: "samples/A-24-elizabethmalarawieczorek15.jpg"
             },
             {
+                artist: "Claude Monet",
+                title: "Water Lilies",
+                url: "samples/water-lilies-1919-2.jpg"
+            },
+            {
+                artist: "Edvard Munch",
+                title: "Scream",
+                url: "samples/munch.scream.jpg"
+            },
+            {
+                artist: "Alfons Mucha",
+                title: "Winter",
+                url: "samples/Alfons_Mucha_-_1896_-_Winter.jpg"
+            },
+            {
+                artist: "Banksy",
+                title: "",
+                url: "samples/banksy_ratgirlzzz1_1000.jpg"
+            },
+            {
+                artist: "Banksy",
+                title: "",
+                url: "samples/colorful-banksy.jpg"
+            },
+            {
                 artist: "Elizabeth Malara",
                 title: "Blooming Tree",
                 url: "samples/A-08-01_Blooming-Tree.jpg"
+            },
+            {
+                artist: "Franklin Carmichael",
+                title: "Mirror Lake",
+                url: "samples/mirror-lake-1929.jpg"
+            },
+            {
+                artist: "Vincent Van Gogh",
+                title: "Wheat Field",
+                url: "samples/Vincent_van_Gogh_-_Wheat_Field_with_Cypresses_-_Google_Art_Project.jpg"
             }
         ]
     },
